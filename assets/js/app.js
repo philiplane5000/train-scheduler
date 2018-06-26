ab02290b26f37262bcc5ad38445f8c140434eb39
philtd5000 <philiptaft9@gmail.com>
2018-06-26 08:44:36 -0700

    not much progress due to bug...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var config = {
+let config = {
     apiKey: "AIzaSyCcOz4jinogYZJKzryxbfY_8TbRYYo8670",
     authDomain: "train-scheduler-37f5e.firebaseapp.com",
     databaseURL: "https://train-scheduler-37f5e.firebaseio.com",
@@ -9,8 +9,8 @@ var config = {
 
   firebase.initializeApp(config);
 
-          //TEST MOMENT.JS:
-          console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));   
+    //TEST MOMENT.JS:
+    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));   
 
   const database = firebase.database();
   const ref = database.ref();
@@ -20,23 +20,56 @@ var config = {
     event.preventDefault();
 
     let name = $('#train-name').val().trim();
-    let dest = $('#destination').val().trim();
+    let dest = $('#dest').val().trim();
     let first = $('#first-train-time').val().trim();
-    let freq = $('#frequency').val().trim();
+    let frequency = $('#frequency').val().trim();
 
     console.log(name);
     console.log(dest);
     console.log(first);
-    console.log(freq);
+    console.log(frequency);
 
+    let newTrain = {
+        trainName: name,
+        destination: dest,
+        firstTrainTime: first,
+        frequency: freq
+    };
 
-    // let momentStart = moment(start, "DD/MM/YYYY").format();
+//     console.log(newTrain);
 
-  ref.push({
-      trainName: name,
-      destination: dest,
-      firstTrainTime: first,
-      frequency: freq
-    })
+  ref.push(newTrain);
 
+    $("#train-name").val("");
+    $("#dest").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+
+})
+
+ref.on('child_added', function (snapshot) {
+    //SIMPLIFY:
+    let value = snapshot.val();
+
+    //RETRIEVE VALUES FROM DATABASE:
+    let train = value.trainName;
+    let destination = value.destination;
+    let frequency = value.frequency;
+    let firstTrainTime = value.firstTrainTime;
+
+    // let firstTrainSecs = moment()
+
+    let now = moment().format('HH:mm');
+    console.log(now);
+
+    $('#train-schedule').append(`
+        <tr>
+            <td id="train">${train}</td>
+            <td id="destination">${destination}</td>
+            <td id="frequency">${frequency}</td>
+            <td id="next-arrival"> - </td>
+            <td id="minutes-away"> - </td>
+        </tr>
+    `)
+    
 })
