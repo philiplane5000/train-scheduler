2e9488cf59a2f1c94e4135dbf4a13a7ad560b68b
philtd5000 <philiptaft9@gmail.com>
2018-06-28 09:50:50 -0700

    styles and color theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,52 +41,52 @@ $('#submit').on('click', function (event) {
 
 })
 
-ref.on('child_added', function (snapshot) {
-    //SIMPLIFY:
-    let value = snapshot.val();
-
-    //RETRIEVE VALUES FROM DATABASE:
-    let train = value.trainName;
-    let destination = value.destination;
-    let frequency = value.frequency;
-    let firstTrainTime = value.firstTrainTime;
-
-    //CONVERT FIRST-TRAIN-TIME into MINUTES/1440 (1)
-    let firstTrainTimeMinutes = moment(firstTrainTime, "HH:mm").format('mm');
-    let firstTrainTimeHour = moment(firstTrainTime, "HH:mm").format('hh');
-    let convertHoursToMinutes = firstTrainTimeHour * 60;
-    let firstTrainMinutesPastMidnight = parseInt(convertHoursToMinutes) + parseInt(firstTrainTimeMinutes);
-
-    console.log(train + " = TRAIN NAME");
-    console.log(firstTrainTime + " = DEPARTS AT")
-    console.log(firstTrainMinutesPastMidnight + " = MINUTES PAST MIDNIGHT OF FIRST TRAIN");
-
-    //CONVERT TIME NOW into MINUTES/1440 (2)
-    let now = moment().format('HH:mm');
-    let timeNowHour = moment(now, "HH:mm").format('HH');
-    let timeNowHoursToMinutes = timeNowHour * 60;
-    let timeNowMinutes = moment(now, "HH:mm").format('mm');
-    let timeNowMinutesPastMidnight = parseInt(timeNowHoursToMinutes) + parseInt(timeNowMinutes);
-
-    console.log(now + " = TIME NOW");
-    console.log(timeNowMinutesPastMidnight + " = TIME NOW MINUTES PAST MIDNIGHT");
-
-    console.log(returnMinutesAway(firstTrainMinutesPastMidnight, timeNowMinutesPastMidnight, frequency) + " = NEXT TRAIN MINUTES");
-    let minutesAway = returnMinutesAway(firstTrainMinutesPastMidnight, timeNowMinutesPastMidnight, frequency);
-
-    let nextArrival = returnNextArrival(minutesAway);
-
-    $('#train-schedule').append(`
-        <tr>
-            <td id="train" class="text-center">${train}</td>
-            <td id="destination" class="text-center">${destination}</td>
-            <td id="frequency" class="text-center">${frequency}</td>
-            <td id="next-arrival" class="text-center"> ${nextArrival} </td>
-            <td id="minutes-away" class="text-center"> ${minutesAway} </td>
-        </tr>
-    `)
-
-})
+    ref.on('child_added', function (snapshot) {
+        //SIMPLIFY:
+        let value = snapshot.val();
+    
+        //RETRIEVE VALUES FROM DATABASE:
+        let train = value.trainName;
+        let destination = value.destination;
+        let frequency = value.frequency;
+        let firstTrainTime = value.firstTrainTime;
+    
+        //CONVERT FIRST-TRAIN-TIME into MINUTES/1440 (1)
+        let firstTrainTimeMinutes = moment(firstTrainTime, "HH:mm").format('mm');
+        let firstTrainTimeHour = moment(firstTrainTime, "HH:mm").format('hh');
+        let convertHoursToMinutes = firstTrainTimeHour * 60;
+        let firstTrainMinutesPastMidnight = parseInt(convertHoursToMinutes) + parseInt(firstTrainTimeMinutes);
+    
+        console.log(train + " = TRAIN NAME");
+        console.log(firstTrainTime + " = DEPARTS AT")
+        console.log(firstTrainMinutesPastMidnight + " = MINUTES PAST MIDNIGHT OF FIRST TRAIN");
+    
+        //CONVERT TIME NOW into MINUTES/1440 (2)
+        let now = moment().format('HH:mm');
+        let timeNowHour = moment(now, "HH:mm").format('HH');
+        let timeNowHoursToMinutes = timeNowHour * 60;
+        let timeNowMinutes = moment(now, "HH:mm").format('mm');
+        let timeNowMinutesPastMidnight = parseInt(timeNowHoursToMinutes) + parseInt(timeNowMinutes);
+    
+        console.log(now + " = TIME NOW");
+        console.log(timeNowMinutesPastMidnight + " = TIME NOW MINUTES PAST MIDNIGHT");
+    
+        console.log(returnMinutesAway(firstTrainMinutesPastMidnight, timeNowMinutesPastMidnight, frequency) + " = NEXT TRAIN MINUTES");
+        let minutesAway = returnMinutesAway(firstTrainMinutesPastMidnight, timeNowMinutesPastMidnight, frequency);
+    
+        let nextArrival = returnNextArrival(minutesAway);
+    
+        $('#train-schedule').append(`
+            <tr>
+                <td id="train" class="text-center">${train}</td>
+                <td id="destination" class="text-center">${destination}</td>
+                <td id="frequency" class="text-center">${frequency}</td>
+                <td id="next-arrival" class="text-center"> ${nextArrival} </td>
+                <td id="minutes-away" class="text-center"> ${minutesAway} </td>
+            </tr>
+        `)
+    
+    })
 
 //FUNCTION WILL RUN TIME NOW IN MINUTES PAST MIDNIGHT AGAINST A FOR LOOP UP TO 1440(MINUTES IN 24 HOURS) WHERE INTERVAL IS EQUAL TO TRAIN FREQUENCY: 
 let returnMinutesAway = function (firstTrainMins, timeNow, interval) {
